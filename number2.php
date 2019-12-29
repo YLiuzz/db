163bb1d31b9f4e98e3cb92b3a0c4162df43fc9a8
Jonathan <Jake10549129@gmail.com>
2019-12-29 15:12:14 +0800

    第二頁

---
+++ number2.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset ="UTF-8">
+<title>Title of the document</title>
+</head>
+
+<body>
+<h1>客戶基本資料</h1>
+
+<form>
+	<p>身分證字號 <input type="text" name="num1" placeholder="Number1"></p>
+	<p>訂貨日期 <input type="text" name="num2" placeholder="Number2"></p>
+	<p>預計遞交日期 <input type="text" name="num3" placeholder="Number3"></p>
+	<p>實際遞交日期 <input type="text" name="num4" placeholder="Number4"></p>
+	<p>訂貨品名 <input type="text" name="num5" placeholder="Number5"></p>
+	<p>單位 <input type="text" name="num6" placeholder="Number6"></p>
+	<p>數量 <input type="text" name="num7" placeholder="Number7"></p>
+	<p>單價 <input type="text" name="num8" placeholder="Number8"></p>
+	<p>訂貨金額 <input type="text" name="num9" placeholder="Number9"></p>
+	<p>供應商名稱 <input type="text" name="num10" placeholder="Number10"></p>
+	<p>供應商編號 <input type="text" name="num11" placeholder="Number11"></p>
+	<button type="submit" name="submit" value="submit">提交資料</button>
+	<p></p>
+
+	
+	<br>
+
+	
+
+</form>
+
+<?php
+	//check if submit botton has been hit
+	if (isset($_GET['submit'])) {
+		$result1 = $_GET['num1'];
+		$result2 = $_GET['num2'];
+		$operator = $_GET['operator'];
+		switch ($operator) {
+					case "None":
+						echo "Error, you need to select operator";
+						break;
+					case "Add":
+						echo $result1 + $result2;
+						break;
+					case "Subtract":
+						echo $result1 - $result2;
+						break;
+					case "Multiply":
+						echo $result1 * $result2;
+						break;
+					case "Divide":
+						echo $result1 / $result2;
+						break;		
+					
+					
+				}		
+	}
+
+?>
+	
+
+</body>
+
+	
+</html>
