69d859de6f34ad834289cf338fc842db1c5f469d
sky <godbless5568855@gmail.com>
2019-12-29 15:37:20 +0800

    3 page

---
+++ number3.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset ="UTF-8">
+<title>公司進貨資料庫</title>
+</head>
+
+<body>
+<h1>公司進貨資料庫</h1>
+
+<form>
+	<p>供應商名稱 <input type="text" name="num1" placeholder="Number1"></p>
+	<p>供應商編號 <input type="text" name="num2" placeholder="Number2"></p>
+	<p>供應商負責人<input type="text" name="num3" placeholder="Number3"></p>
+	<p>進貨品名<input type="text" name="num4" placeholder="Number4"></p>
+	<p>進貨數量 <input type="text" name="num5" placeholder="Number5"></p>
+	<p>進貨單位 <input type="text" name="num6" placeholder="Number6"></p>
+	<p>進貨單價 <input type="text" name="num7" placeholder="Number7"></p>
+	<p>小計 <input type="text" name="num8" placeholder="Number8"></p>
+	<p>庫存位置 <input type="text" name="num9" placeholder="Number9"></p>
+	<p>規格 <input type="text" name="num10" placeholder="Number10"></p>
+	<p>進貨日期 <input type="text" name="num11" placeholder="Number11"></p>
+		<button type="submit" name="submit" value="submit">提交資料</button>
+	<p></p>
+
+	
+	<br>
+
+	
+
+</form>
+
+
+	
+
+</body>
+
+	
+</html>
